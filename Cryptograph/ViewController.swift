88dc44adb8856d1add3e20dbce2f9d62c7d0a053
a063mg <alekseev.artem700@gmail.com>
2017-08-03 22:29:08 +0300

    Initial Commit

---
+++ Cryptograph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cryptograph
+//
+//  Created by Артем on 03.08.17.
+//  Copyright © 2017 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
