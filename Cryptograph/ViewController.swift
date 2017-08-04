f991cfa48a441b64518441a0b4f1e2a0385ac5f8
a063mg <alekseev.artem700@gmail.com>
2017-08-04 22:40:03 +0300

    Save

--- Cryptograph/ViewController.swift
+++ Cryptograph/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,62 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func chooseImageFromLibrary(_ sender: UIBarButtonItem) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.modalPresentationStyle = .popover
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    
+        imagePicker.popoverPresentationController?.barButtonItem = sender
+        
+//        do {
+//            let content = try String(contentsOf:URL(string: "https://cryptograph.000webhostapp.com/encode_images/test.txt")!)
+//            print(content)
+//        }
+//        catch{
+//            print(error)
+//        }
+//        
+//        let imgURL = "https://cryptograph.000webhostapp.com/encode_images/unicredit%20(1).jpg" // or jpg
+//        self.imageView.setImageFromURl(stringImageUrl: imgURL)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func Cancel(_ sender: Any) {
+        imageView.image = nil
+    }
+    
+    @IBAction func saveImage(_ sender: Any) {
+         UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                            didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        imageView.contentMode = .scaleAspectFit //3
+        imageView.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5
+    }
 
 }
 
